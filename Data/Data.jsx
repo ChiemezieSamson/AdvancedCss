c5d11dbf87f9062a0311f5d34ef2f9ca63267c7d
nebeolisa chiemezie samson <daddygreenarrow44@gmail.com>
2024-10-07 22:21:07 +0900

    updated the HomeData Array with real image url

--- Data/Data.jsx
+++ Data/Data.jsx
@@ -2,7 +2,7 @@ export const HomeData = [
   {
     "id": 0,
     "title": "G7 Summit Focuses on Global Economic Recovery",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751308203393106/powerplant.png?ex=66a9ece9&is=66a89b69&hm=7e82cbbad1af32178c3d423a81f0b6e9bd33dab361ebb52eaf32453c83f118e4&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/8e/21/91/8e21916c7d82e05a054475989c195e3e.jpg",
     "textcontent": "Leaders of the G7 nations have gathered in Tokyo to discuss strategies for accelerating global economic recovery post-pandemic.",
     "source_name": "Economy",
     "publication_date": "2024-06-01"
@@ -10,7 +10,7 @@ export const HomeData = [
   {
     "id": 1,
     "title": "UN Climate Report Warns of Accelerating Global Warming",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751416320229428/water.png?ex=66a9ed03&is=66a89b83&hm=e77093104423efddb1bfa275a27b724862fccd75cf0585d30e70fdd95e4f97bb&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/17/42/67/1742677e0a7934aa139c0aa036196ba5.jpg",
     "textcontent": "The latest UN climate report highlights the urgent need for global action to combat the accelerating effects of global warming.",
     "source_name": "Environment",
     "publication_date": "2024-06-05"
@@ -18,7 +18,7 @@ export const HomeData = [
   {
     "id": 2,
     "title": "Historic Peace Agreement Signed in Middle East",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751307121262705/skyscraper.png?ex=66a9ece9&is=66a89b69&hm=6f9517b7018514266e19134978d03f7c8c6fa161ff0729bbb31d005134831a48&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/b7/d6/27/b7d62769b9ae7c875975bd0780d78687.jpg",
     "textcontent": "After decades of conflict, a historic peace agreement has been signed in the Middle East, marking a new era of hope and stability.",
     "source_name": "Politics",
     "publication_date": "2024-06-08"
@@ -26,7 +26,7 @@ export const HomeData = [
   {
     "id": 3,
     "title": "Breakthrough in Cancer Research Announced",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751308459376701/port-and-ship.png?ex=66a9ece9&is=66a89b69&hm=6f3c7bed66191cb4aa4df8426775965ef5e7619bc3998ffca240bc4689ade1a2&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/736x/85/4e/a0/854ea0ea818b464eedbd00b0ace1e3ec.jpg",
     "textcontent": "Scientists have announced a major breakthrough in cancer research, potentially leading to more effective treatments.",
     "source_name": "Health",
     "publication_date": "2024-06-10"
@@ -34,7 +34,7 @@ export const HomeData = [
   {
     "id": 4,
     "title": "Global Tech Conference Highlights AI Innovations",
-    "image": "https://images-ext-1.discordapp.net/external/Edipq4OdtPzSErS2rKEBaMmx4R3q2WmR6bqv8l84xI0/https/megaproject-type-images.s3.ap-northeast-2.amazonaws.com/factory.png?format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/736x/bc/e1/71/bce17118114a9d98a3134a4bb3a68deb.jpg",
     "textcontent": "The annual Global Tech Conference showcased the latest innovations in artificial intelligence and their potential applications.",
     "source_name": "Technology",
     "publication_date": "2024-06-12"
@@ -42,7 +42,7 @@ export const HomeData = [
   {
     "id": 5,
     "title": "Wildfires Ravage Parts of Australia",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751307478040646/road-bridge.png?ex=66a9ece9&is=66a89b69&hm=6104bae8bfb627dff6c09a81fae88b4b255649933e9d9625dd0cd8384728e07d&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/e2/d4/fc/e2d4fc78d15a0be0e495b0687cd9ad99.jpg",
     "textcontent": "Severe wildfires have broken out in parts of Australia, causing widespread damage and prompting evacuations.",
     "source_name": "Disaster",
     "publication_date": "2024-06-15"
@@ -50,7 +50,7 @@ export const HomeData = [
   {
     "id": 6,
     "title": "Global Stock Markets Experience Volatility",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751308908302410/oil-gas.png?ex=66a9ece9&is=66a89b69&hm=d4fa99b25ed72081c4744121d10df923c32dc419e6b861fb92724f52ca8e1ca1&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/38/d4/d7/38d4d70886c8a2dd73f3e5f1497c8f73.jpg",
     "textcontent": "Stock markets around the world have experienced significant volatility amid economic uncertainties.",
     "source_name": "Economy",
     "publication_date": "2024-06-18"
@@ -58,7 +58,7 @@ export const HomeData = [
   {
     "id": 7,
     "title": "New Species Discovered in the Amazon Rainforest",
-    "image": "https://images-ext-1.discordapp.net/external/RNJlywsnu8bEC3XUAgotpFkBCMYOH9ccy1ngQn1V25g/https/megaproject-type-images.s3.ap-northeast-2.amazonaws.com/airport.png?format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/736x/21/fd/df/21fddf788598e7b4fe4a563e7d793db1.jpg",
     "textcontent": "Scientists have discovered several new species in the Amazon Rainforest, highlighting the region's rich biodiversity.",
     "source_name": "Science",
     "publication_date": "2024-06-20"
@@ -66,7 +66,7 @@ export const HomeData = [
   {
     "id": 8,
     "title": "Olympics 2024: Preparations in Full Swing",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751307796811806/railway.png?ex=66a9ece9&is=66a89b69&hm=9254a70830f75063bf85bb65e4fa8a053d070bbc88cde07659d59da1f41e4b51&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/736x/85/9c/76/859c76d7d9b56ec29fe4869f69735634.jpg",
     "textcontent": "Preparations for the 2024 Summer Olympics are in full swing, with host city Paris readying venues and infrastructure.",
     "source_name": "Sports",
     "publication_date": "2024-06-22"
@@ -74,7 +74,7 @@ export const HomeData = [
   {
     "id": 9,
     "title": "Global Efforts to Combat Plastic Pollution Intensify",
-    "image": "https://images-ext-1.discordapp.net/external/PPFZeW-_I9BiSjwhfi8kuRtbLRGFZKbeBG-ymtQdrPI/https/megaproject-type-images.s3.ap-northeast-2.amazonaws.com/mining.png?format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/ae/96/f1/ae96f1572d810acedff0363098ebb9f2.jpg",
     "textcontent": "Countries around the world are stepping up efforts to combat plastic pollution, implementing new policies and initiatives.",
     "source_name": "Environment",
     "publication_date": "2024-06-25"
@@ -82,7 +82,7 @@ export const HomeData = [
   {
     "id": 10,
     "title": "Major Cyber Attack Targets Global Corporations",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751416047472705/stadium.png?ex=66a9ed03&is=66a89b83&hm=a73640a356d4fdbf159bee0bdf2d08c1269a66e9f24fe5850f6861052eaa4f08&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/87/38/21/873821c050b3de2ce8c5b7164b848854.jpg",
     "textcontent": "A major cyber attack has targeted global corporations, compromising sensitive data and disrupting operations.",
     "source_name": "Technology",
     "publication_date": "2024-06-28"
@@ -90,7 +90,7 @@ export const HomeData = [
   {
     "id": 11,
     "title": "World Leaders Discuss Global Security Challenges",
-    "image": "https://media.discordapp.net/attachments/1253323541768830997/1267751309264683040/mixed-use.png?ex=66a9ece9&is=66a89b69&hm=76c8fb887d05fb807b460dc3aeb5dbdc80dae24b884e7a11b42aee0734125ac1&=&format=webp&quality=lossless",
+    "image": "https://i.pinimg.com/564x/a7/94/71/a79471defe4591601c826b3d4a48a55a.jpg",
     "textcontent": "World leaders have convened to discuss pressing global security challenges and potential collaborative solutions.",
     "source_name": "Politics",
     "publication_date": "2024-06-30"
@@ -98,7 +98,7 @@ export const HomeData = [
   {
     "id": 12,
     "title": "Advancements in Renewable Energy Technologies",
-    "image": "https://i.pinimg.com/236x/af/e0/99/afe0991645af3770823aaae6ffbf9c0e.jpg",
+    "image": "https://i.pinimg.com/736x/13/c5/76/13c5760eceea6843a76c2444c6f08521.jpg",
     "textcontent": "Significant advancements in renewable energy technologies are paving the way for a more sustainable future.",
     "source_name": "Technology",
     "publication_date": "2024-07-02"
@@ -118,5 +118,205 @@ export const HomeData = [
     "textcontent": "A major breakthrough in quantum computing has been announced, potentially revolutionizing various industries.",
     "source_name": "Science",
     "publication_date": "2024-07-08"
+  },
+  {
+    "id": 15,
+    "title": "Solar Energy Adoption Increases in Developing Countries",
+    "image": "https://i.pinimg.com/564x/89/a2/93/89a2931c6ff234848bdb54a638475f34.jpg",
+    "textcontent": "The adoption of solar energy is accelerating in developing countries as governments prioritize renewable resources.",
+    "source_name": "Environment",
+    "publication_date": "2024-06-07"
+  },
+  {
+    "id": 16,
+    "title": "New Tech Innovations Unveiled at Annual Conference",
+    "image": "https://i.pinimg.com/564x/b2/75/33/b27533b4177c41545dfb178aa16f2ff7.jpg",
+    "textcontent": "Innovative products from AI to robotics were showcased at this year's tech conference, drawing global attention.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-08"
+  },
+  {
+    "id": 17,
+    "title": "Global Food Crisis Worsens Due to Climate Change",
+    "image": "https://i.pinimg.com/564x/0d/2e/89/0d2e897471563d9251c32ecf03763bba.jpg",
+    "textcontent": "Agricultural sectors worldwide are being hit hard by the effects of climate change, exacerbating the global food crisis.",
+    "source_name": "Economy",
+    "publication_date": "2024-06-09"
+  },
+  {
+    "id": 18,
+    "title": "SpaceX Launches New Rocket with Reusable Components",
+    "image": "https://i.pinimg.com/564x/b6/e6/a7/b6e6a70df099d39b1e06d8a7a3c5e1cd.jpg",
+    "textcontent": "SpaceX successfully launched its latest rocket, marking another milestone in reusable space technology.",
+    "source_name": "Science",
+    "publication_date": "2024-06-10"
+  },
+  {
+    "id": 19,
+    "title": "Global Supply Chain Faces New Challenges Amid Political Tensions",
+    "image": "https://i.pinimg.com/564x/14/94/c7/1494c716d670e463f570f43eb83a0343.jpg",
+    "textcontent": "The global supply chain faces unprecedented challenges due to rising political tensions in key regions.",
+    "source_name": "Economy",
+    "publication_date": "2024-06-11"
+  },
+  {
+    "id": 20,
+    "title": "New Study Highlights Benefits of Meditation on Mental Health",
+    "image": "https://i.pinimg.com/564x/2c/8c/3f/2c8c3fee236dc10606c323bca207f16f.jpg",
+    "textcontent": "Researchers have found that regular meditation can significantly improve mental well-being and reduce stress.",
+    "source_name": "Health",
+    "publication_date": "2024-06-12"
+  },
+  {
+    "id": 21,
+    "title": "World Leaders Commit to Reducing Carbon Emissions",
+    "image": "https://i.pinimg.com/736x/5a/05/ad/5a05ad8df10ce23b22cbb80974522a2e.jpg",
+    "textcontent": "In a landmark agreement, world leaders pledged to significantly reduce carbon emissions over the next decade.",
+    "source_name": "Environment",
+    "publication_date": "2024-06-13"
+  },
+  {
+    "id": 22,
+    "title": "Breakthrough in Electric Vehicle Battery Technology",
+    "image": "https://i.pinimg.com/736x/ca/4e/b0/ca4eb0ffc44264a91ffcc5fffffe3555.jpg",
+    "textcontent": "A new advancement in electric vehicle battery technology promises longer range and faster charging times.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-14"
+  },
+  {
+    "id": 23,
+    "title": "Rising Housing Prices Cause Concern Among First-Time Buyers",
+    "image": "https://i.pinimg.com/564x/7a/1c/c4/7a1cc487d0e0c9398a5ba73b82038908.jpg",
+    "textcontent": "First-time homebuyers are struggling as housing prices continue to rise, especially in urban areas.",
+    "source_name": "Real Estate",
+    "publication_date": "2024-06-15"
+  },
+  {
+    "id": 24,
+    "title": "Scientists Develop New Method for Recycling Plastics",
+    "image": "https://i.pinimg.com/564x/50/90/22/509022166e37f036d5873713057a7f34.jpg",
+    "textcontent": "A group of scientists has developed a groundbreaking method for recycling plastics that could reduce pollution.",
+    "source_name": "Science",
+    "publication_date": "2024-06-16"
+  },
+  {
+    "id": 25,
+    "title": "AI Adoption in Healthcare Continues to Expand",
+    "image": "https://i.pinimg.com/736x/f1/2c/bc/f12cbc8686ec286e18e9f95a56c7137c.jpg",
+    "textcontent": "Healthcare providers are increasingly adopting artificial intelligence technologies to enhance patient care.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-17"
+  },
+  {
+    "id": 26,
+    "title": "Tourism Industry Sees Boom Following Global Travel Resumptions",
+    "image": "https://i.pinimg.com/564x/5f/1a/ff/5f1aff36c8f951f7419b4e0ccf16b7af.jpg",
+    "textcontent": "Tourism industries worldwide are experiencing a significant boom after global travel restrictions were lifted.",
+    "source_name": "Economy",
+    "publication_date": "2024-06-18"
+  },
+  {
+    "id": 27,
+    "title": "Virtual Reality Gains Momentum in Education",
+    "image": "https://i.pinimg.com/564x/8f/b1/b3/8fb1b34fc46cad9acec17fd810c86317.jpg",
+    "textcontent": "Virtual reality technology is gaining momentum in educational settings, offering immersive learning experiences.",
+    "source_name": "Education",
+    "publication_date": "2024-06-19"
+  },
+  {
+    "id": 28,
+    "title": "Cryptocurrency Regulation Talks Intensify Among World Governments",
+    "image": "https://i.pinimg.com/736x/2d/2e/22/2d2e22aa4501ca58182fc752cd2be239.jpg",
+    "textcontent": "World governments are intensifying discussions on regulating cryptocurrencies amid concerns over market volatility.",
+    "source_name": "Finance",
+    "publication_date": "2024-06-20"
+  },
+  {
+    "id": 29,
+    "title": "Electric Cars Expected to Dominate Market by 2030",
+    "image": "https://i.pinimg.com/736x/82/0c/c0/820cc0de717f4ee0c0a8e7fd15e9f4f1.jpg",
+    "textcontent": "Experts predict that electric cars will dominate the global auto market by 2030 due to decreasing battery costs and environmental concerns.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-21"
+  },
+  {
+    "id": 30,
+    "title": "Global Hunger Crisis Worsens Despite International Efforts",
+    "image": "https://i.pinimg.com/564x/ea/19/ce/ea19ce8f557452b9845b0bd4afa9b45a.jpg",
+    "textcontent": "Despite global initiatives to end hunger, food insecurity continues to rise in certain regions, exacerbated by economic and political instability.",
+    "source_name": "Economy",
+    "publication_date": "2024-06-22"
+  },
+  {
+    "id": 31,
+    "title": "New Renewable Energy Sources Show Promise for Future",
+    "image": "https://i.pinimg.com/564x/86/7c/bb/867cbb8ab44ec554bb44d623210210a2.jpg",
+    "textcontent": "Innovations in renewable energy sources, including tidal and geothermal energy, offer new hope for sustainable power generation.",
+    "source_name": "Environment",
+    "publication_date": "2024-06-23"
+  },
+  {
+    "id": 32,
+    "title": "Scientists Decode Ancient Texts Using AI",
+    "image": "https://i.pinimg.com/564x/24/27/c1/2427c1567a9ef20e9db10bcf6660fd1f.jpg",
+    "textcontent": "A team of scientists has used AI to decode previously unreadable ancient texts, shedding new light on historical events.",
+    "source_name": "Science",
+    "publication_date": "2024-06-24"
+  },
+  {
+    "id": 33,
+    "title": "Advances in Telemedicine Are Reshaping Healthcare",
+    "image": "https://i.pinimg.com/564x/db/aa/6d/dbaa6d3b191888a2afbd0dd9cac9145a.jpg",
+    "textcontent": "Telemedicine technology continues to make healthcare more accessible, especially for patients in remote locations.",
+    "source_name": "Health",
+    "publication_date": "2024-06-25"
+  },
+  {
+    "id": 34,
+    "title": "Global Economies Brace for Impact of Rising Oil Prices",
+    "image": "https://i.pinimg.com/564x/89/cb/4f/89cb4f4c51771968066d7befd4d9e8de.jpg",
+    "textcontent": "Rising oil prices are sparking concerns among global economies, which fear the impact on inflation and energy markets.",
+    "source_name": "Economy",
+    "publication_date": "2024-06-26"
+  },
+  {
+    "id": 35,
+    "title": "Tech Giants Collaborate on AI Ethics Standards",
+    "image": "https://i.pinimg.com/564x/7a/90/a4/7a90a447374e70e7c911e9c78dbf739b.jpg",
+    "textcontent": "Leading tech companies have announced a joint initiative to establish global ethics standards for artificial intelligence development.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-02"
+  },
+  {
+    "id": 36,
+    "title": "Climate Change Takes Center Stage at UN Summit",
+    "image": "https://i.pinimg.com/564x/f4/d6/cb/f4d6cbe317911722bd1e38ecd9863b1b.jpg",
+    "textcontent": "Global leaders are meeting to discuss urgent actions needed to mitigate the effects of climate change, with a focus on renewable energy.",
+    "source_name": "Environment",
+    "publication_date": "2024-06-03"
+  },
+  {
+    "id": 37,
+    "title": "Breakthrough in Cancer Research Announced",
+    "image": "https://i.pinimg.com/564x/0a/aa/53/0aaa53b7278efe597a1e675d6c19116a.jpg",
+    "textcontent": "Scientists have revealed a major breakthrough in cancer research that could lead to more effective treatments and improved patient outcomes.",
+    "source_name": "Health",
+    "publication_date": "2024-06-04"
+  },
+  {
+    "id": 38,
+    "title": "Stock Markets Show Signs of Recovery Amid Global Uncertainty",
+    "image": "https://i.pinimg.com/564x/dc/5d/3e/dc5d3e2649c9c539228e60d5554f48a9.jpg",
+    "textcontent": "Global stock markets have begun to recover, offering hope to investors after months of volatility and uncertainty.",
+    "source_name": "Finance",
+    "publication_date": "2024-06-05"
+  },
+  {
+    "id": 39,
+    "title": "Advancements in Quantum Computing Pave the Way for Innovation",
+    "image": "https://i.pinimg.com/564x/4b/6f/df/4b6fdf9770899dbe0b5af5b11555bddd.jpg",
+    "textcontent": "Researchers have made significant strides in quantum computing, a technology poised to revolutionize industries from cybersecurity to healthcare.",
+    "source_name": "Technology",
+    "publication_date": "2024-06-06"
   }
 ]
